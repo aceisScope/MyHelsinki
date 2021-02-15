fa25556d5a31ef8f83275ce2f2ea35d5759a4684
Binghui Liu <bertieliu52@gmail.com>
2021-02-15 17:34:31 +0200

    update app title

--- city-app/src/App.js
+++ city-app/src/App.js
@@ -1,9 +1,14 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import Navibar from './components/Navibar';
 import Places from './components/Places';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "MyHelsinki";
+  });
+  
   return (
     <Router>
       <Navibar/>
